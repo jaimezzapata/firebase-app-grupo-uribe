9327dfdd62112fc94588e65a02441be58b19de5a
jaimezzapata <zapataval2304@gmail.com>
2024-05-02 15:53:03 -0500

    Se agregan estilos al panel

--- src/components/helpers/SideBar.jsx
+++ src/components/helpers/SideBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Asumiendo que estás utilizando React Router
+import { Link } from 'react-router-dom';
+import './SideBar.css'
 
 const SideBar = () => {
     return (
         <header className="sidebar">
             <h2>Panel de Control</h2>
-            <ul>
-                <li><Link to="/home">Dashboard</Link></li>
-                <li><Link to="/usuarios">Usuarios</Link></li>
-                <li><Link to="/">Cerrar Sesión</Link></li>
+            <ul className='opciones'>
+                <li className='animated-btn'><Link to="/home">Dashboard</Link></li>
+                <li className='animated-btn'><Link to="/usuarios">Usuarios</Link></li>
+                <li className='animated-btn'><Link to="/">Cerrar Sesión</Link></li>
             </ul>
         </header>
     );
